7d2a4f2d94c958dad12c1caeb529d44a12bf6f48
Max Kuzmovych <max.kuzmovych@botscrew.com>
2021-01-27 22:13:38 +0200

    pdate lambda

--- lambda/index.js
+++ lambda/index.js
@@ -1,22 +1,26 @@
 const mysql = require('mysql');
 
+const QUERY = 'SHOW DATABASES;';
+
 const connection = mysql.createConnection({
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    port: process.env.RDS_PORT,
+    host: 'flightdb.cdmh7o8qmr8i.us-east-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Ghs27hYsdj12',
+    port: '3306',
 });
 
 exports.handler = async (event) => {
+    console.log(event);
     try {
         const data = await new Promise((resolve, reject) => {
             event.Records.forEach(record => {
                 // Kinesis data is base64 encoded so decode here
                 var payload = JSON.parse(Buffer.from(record.kinesis.data, 'base64').toString('ascii'));
-                const query = `INSERT INTO lab_data.info (event, device, submission_date) VALUE (${payload.event}, ${payload.device}, ${payload.timestamp});`;
+                console.log('Iserting new value:', payload);
+                const query = `INSERT INTO lab_data.info (event, device, submission_date) VALUE ('${payload.event}', '${payload.device}', '${payload.timestamp}');`;
                 connection.query(query, function (err, result) {
                     if (err) {
-                        console.log("Error->" + err);
+                        console.log("Error:" + err);
                         reject(err);
                     }
                     resolve(result);
@@ -29,6 +33,7 @@ exports.handler = async (event) => {
             body: JSON.stringify('Success')
         }
     } catch (err) {
+        console.log('Error:', err);
         return {
             statusCode: 400,
             body: err.message
